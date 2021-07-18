b7b36581bffefe32fbfe51fa2ac2299544d5c13f
nmnethaji8 <59523371+nmnethaji8@users.noreply.github.com>
2021-07-18 14:48:50 +0530

    Create 1.modCommon.h

---
+++ modCommon.cu/1.modCommon.h
@@ -0,0 +1,19 @@
+#ifndef MODCOMMON
+#define MODCOMMON
+namespace COMMONMOD
+{
+    extern long int LNODE;
+
+    extern long int *NODEID, *NWALLID;
+    extern long int ICELLX, ICELLY, ICELLZ;
+
+    extern double *COORX, *COORY, *COORZ;
+    extern double *SNX, *SNY, *SNZ;
+    extern double *SMX, *SMY, *SMZ;
+    extern double *SSX, *SSY, *SSZ;
+    extern double *TANK_A;
+
+    extern double RCELL;
+    void INITCOMMONMOD();
+}
+#endif
