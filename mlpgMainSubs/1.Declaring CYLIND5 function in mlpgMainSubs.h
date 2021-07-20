39064ac3157c6ecc0925a11bc3e3e5a7416dfe55
nmnethaji8 <59523371+nmnethaji8@users.noreply.github.com>
2021-07-20 11:27:23 +0530

    Create 1.Declaring CYLIND5 function in mlpgMainSubs.h

---
+++ mlpgMainSubs/1.Declaring CYLIND5 function in mlpgMainSubs.h
@@ -0,0 +1,7 @@
+#ifndef MLPGMAINSUBS
+#define MLPGMAINSUBS
+#include<string>
+void CYLIND5(std::string , long int &, long int &,
+double *, double *, double *, double , double ,
+double , double);
+#endif
